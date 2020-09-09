b45d18378a1e98e753063e99d149b614cab282e4
Hemanth <viruhemanth@gmail.com>
2020-09-09 19:33:11 +0530

    checking hashrouter on vercel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, HashRouter as Router } from "react-router-dom";
 
 import Navbar from "./shared/Navbar";
 import NotFound from "./pages/not-found";
