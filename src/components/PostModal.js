cdddde189f5ca7dba133ad9c8ccf5f097bc2b479
Hemanth <viruhemanth@gmail.com>
2020-09-16 19:57:30 +0530

    Post Modal Bug fix

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "react-modal";
 import { useHistory } from "react-router-dom";
